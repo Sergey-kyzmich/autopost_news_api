d5a6cf434e5c2a005448574194d16f74ca3925bd
Sergey <ksr-sochi@mail.ru>
2025-06-07 20:19:21 +0300

    test wersion

---
+++ src/test/java/com/ai_agents/autopost_news_api/AutopostNewsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_agents.autopost_news_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutopostNewsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
