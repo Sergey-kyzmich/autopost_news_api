a04b847287e106adec7990f5faf2eb71a1184f45
Sergey <161880791+Sergey-kyzmich@users.noreply.github.com>
2025-06-10 00:13:49 +0300

    Первая рабочая версия gigachat))

--- src/test/java/com/ai_agents/autopost_news_api/CompletionConversationExample.java
+++ src/test/java/com/ai_agents/autopost_news_api/CompletionConversationExample.java
@@ -8,17 +8,12 @@ import java.io.InputStream;
 import java.util.*;
 // import java.io.*;
 
-import chat.giga.client.GigaChatClient;
+import chat.giga.langchain4j.GigaChatChatModel;
 import chat.giga.client.auth.AuthClient;
 import chat.giga.client.auth.AuthClientBuilder;
-import chat.giga.http.client.HttpClientException;
+import chat.giga.langchain4j.GigaChatChatRequestParameters;
 import chat.giga.model.ModelName;
 import chat.giga.model.Scope;
-import chat.giga.model.completion.ChatMessage;
-import chat.giga.model.completion.CompletionRequest;
-import chat.giga.model.completion.CompletionResponse;
-import chat.giga.model.completion.ChatMessageRole;
-
 public class CompletionConversationExample {
 
     public static void main(String[] args) throws IOException {
@@ -28,43 +23,24 @@ public class CompletionConversationExample {
         final String KEY = config[0];
         final String ID = config[1];
         System.out.println(KEY);
-        GigaChatClient client = GigaChatClient.builder()
-                .logRequests(true)
-                .logResponses(true)
-                .authClient(AuthClient.builder()
-                        .withOAuth(AuthClientBuilder.OAuthBuilder.builder()
-                                .scope(Scope.GIGACHAT_API_PERS)
-                                .clientId(ID)
-                                .clientSecret(KEY)
-                                .build())
+
+        GigaChatChatModel model = GigaChatChatModel.builder()
+        .defaultChatRequestParameters(GigaChatChatRequestParameters.builder()
+                .modelName(ModelName.GIGA_CHAT_PRO)
+                .build())
+        .authClient(AuthClient.builder()
+                .withOAuth(AuthClientBuilder.OAuthBuilder.builder()
+                        .scope(Scope.GIGACHAT_API_PERS)
+                        .authKey(KEY)
                         .build())
-                .build();
+                .build())
+        .logRequests(true)
+        .logResponses(true)
+        .build();
 
-        CompletionRequest.CompletionRequestBuilder builder = CompletionRequest.builder()
-                .model(ModelName.GIGA_CHAT_PRO)
-                .message(ChatMessage.builder()
-                        .content("Отвечай как программист")
-                        .role(ChatMessageRole.SYSTEM)
-                        .build())
-                .message(ChatMessage.builder()
-                        .content("Что мне не надо делать, чтобы случайно не захватить мир?")
-                        .role(ChatMessageRole.USER).build());
 
-        try {
-            for (int i = 0; i < 4; i++) {
-                CompletionRequest request = builder.build();
-                CompletionResponse response = client.completions(request);
-                System.out.println(response);
-
-                response.choices().forEach(e -> builder.message(e.message().ofAssistantMessage()));
-
-                builder.message(ChatMessage.builder()
-                        .content("А почему так? Будь еще более точным в формулировках")
-                        .role(ChatMessageRole.USER).build());
-            }
-        } catch (HttpClientException ex) {
-            System.out.println(ex.statusCode() + " " + ex.bodyAsString());
-        }
+      String answer = model.chat("ТЫ кто?");
+      System.out.println(answer); // Pulp Fiction, Kill Bill, etc.
     }
 
 
